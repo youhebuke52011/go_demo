7664522fc5224066ea40cd6108bf6e67f90c6cf1
lihuoquan <lihuoquan@youmi.net>
2019-10-12 17:11:00 +0800

    heap

--- heap/main.go
+++ heap/main.go
@@ -132,7 +132,7 @@ func main() {
 
 	// 对整个arr数组使用HeapSort2排序
 	// HeapSort2, 借助我们的heapify过程创建堆
-	// 此时, 创建堆的过程时间复杂度为O(n), 将所有元素依次从堆中取出来, 实践复杂度为O(nlogn)
+	// 此时, 创建堆的过程时间复杂度为O(n)(O(n)+O(logn),但O(logn远小于n,所以可以忽略)), 将所有元素依次从堆中取出来, 实践复杂度为O(nlogn)
 	// 堆排序的总体时间复杂度依然是O(nlogn), 但是比HeapSort1性能更优, 因为创建堆的性能更优
 	maxHeap = InitHeapByHeapify(arr)
 	for i := 1; i < n; i++ {
